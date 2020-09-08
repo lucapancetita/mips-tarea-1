1ff47792ff6342ba9f69440c8ff6189cf65dcca5
lucapancetita <66691339+lucapancetita@users.noreply.github.com>
2020-09-08 14:37:47 -0300

    Add files via upload

--- factorial.asm
+++ factorial.asm
@@ -11,19 +11,19 @@ prompt: .asciiz "Ingrese un numero\n"
 
     li $v0, 5       # sycall code para leer un int
     syscall         # leemos el entero y el resultado esta en $v0
-    move $t0, $v0   # movemos lo de v0 a s0 (el resultado de leer el entero)
+    move $t0, $v0   # movemos lo de v0 a t0 (el resultado de leer el entero)
 
     addi $a0, $t0, -1 #le resto al numero ingresado 1
 
     loop:
     beq  $a0, $zero, exit
-    mul $t0, $t0, $a0 #mutiplico 3*3 y lo guardo en t1
+    mul $t0, $t0, $a0 #mutiplico  y lo guardo en t1
     addi $a0, $a0, -1 #le resto al numero ingresado 1
     j loop
 
     exit:
     li $v0, 1
-    move $a0, $t0 #muevo lo que esta en t1 a a0
+    move $a0, $t0 #muevo lo que esta en t0 a a0
     syscall
 
 
